7afbe00144dfcaf8668db2b8624a732f9bc44ed5
Gabriel2392 <gabriel824m@gmail.com>
2023-03-05 11:05:59 -0300

    ud: send message as plain text if markdown failed

--- AstrakoBot/modules/ud.py
+++ AstrakoBot/modules/ud.py
@@ -5,7 +5,7 @@ from AstrakoBot.modules.helper_funcs.misc import delete
 from AstrakoBot.modules.sql.clear_cmd_sql import get_clearcmd
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, run_async
-
+import re
 
 def ud(update: Update, context: CallbackContext):
     message = update.effective_message
@@ -18,7 +18,15 @@ def ud(update: Update, context: CallbackContext):
         reply_text = f'*{text}*\n\n{results["list"][0]["definition"]}\n\n_{results["list"][0]["example"]}_'
     except:
         reply_text = "No results found."
-    delmsg = message.reply_text(reply_text, parse_mode=ParseMode.MARKDOWN)
+    try:
+        delmsg = message.reply_text(reply_text, parse_mode=ParseMode.MARKDOWN)
+    except:
+        reply_text = re.sub(r'\*(.*?)\*', r'\1', reply_text)
+        plain_text = reply_text.split('\n')
+        for i in range(1, len(plain_text)):
+            plain_text[i] = plain_text[i].replace('_', '')
+        plain_text = '\n'.join(plain_text)
+        delmsg = message.reply_text(plain_text)
 
     cleartime = get_clearcmd(chat.id, "ud")
 
