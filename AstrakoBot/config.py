37e3793d7ca80ef53a27f10352635f4c18b90079
Ruined Soul <76905260+ruined-soul@users.noreply.github.com>
2024-09-21 21:50:29 +0530

    Update config.py

--- AstrakoBot/config.py
+++ AstrakoBot/config.py
@@ -17,7 +17,7 @@ class Config(object):
 
     API_ID = 123456  # integer value, dont use ""
     API_HASH = "awoo"
-    TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
+    TOKEN = "7997707191:AAGDz7Ffdw-0H-P4YvvZr9X-a-LyYnJRLDE"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
     OWNER_ID = 123456789  # If you dont know, run the bot and do /id in your private chat with it, also an integer
     OWNER_USERNAME = "awoo"
     SUPPORT_CHAT = "awoo"  # Your own group for support, do not add the @
