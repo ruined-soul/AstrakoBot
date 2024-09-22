80948525b49174a2e8e0181a6d4ddce631caa82c
Ruined Soul <76905260+ruined-soul@users.noreply.github.com>
2024-09-22 11:44:07 +0530

    Update config.py

--- AstrakoBot/config.py
+++ AstrakoBot/config.py
@@ -52,7 +52,6 @@ class Config(object):
     WHITELIST_USERS = get_user_list("elevated_users.json", "whitelists")
     DONATION_LINK = None  # EG, paypal
     CERT_PATH = None
-    PORT = 5000
     DEL_CMDS = True  # Delete commands that users dont have access to, like delete /ban if a non admin uses it.
     STRICT_GBAN = True
     WORKERS = (
